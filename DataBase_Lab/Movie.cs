4f073d3e0b2ea311e0580c98d07253f99c455f57
kmerck88 <Kevin.Merck88@gmail.com>
2022-07-27 18:23:59 -0400

    intial commit

---
+++ DataBase_Lab/Movie.cs
@@ -0,0 +1,36 @@
+﻿// Build a Movie class with two properties:  title and category. Both of these properties should be strings. 
+// Create a constructor that takes two parameters: title and category.
+
+class Movie
+{
+  public string Title { get; set; }
+  public string Category { get; set; }
+
+  public Movie(string title, string category)
+  {
+    Title = title;
+    Category = category;
+  }
+  //in your Main, create a List<Movie> and store at least 10 movies there.
+
+  List<Movie> movies = new List<Movie>();
+
+  public Movie(List<Movie> movies)
+  {
+    this.movies = movies;
+  }
+
+  //in your Main, iterate through your List of Movies and print out the title and category of each movie.
+  //Create a foreach loop that iterates through the list and prints out the title and category of each movie.
+
+  public void PrintMovies()
+  {
+    foreach (Movie movie in movies)
+    {
+      Console.WriteLine($"{movie.Title} is a {movie.Category} movie.");
+    }
+    {
+      Console.WriteLine("");
+    }
+  }
+}
